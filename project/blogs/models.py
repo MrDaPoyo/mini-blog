c5306c2e576e6f8555996a0e77df9c5b92c9b7c4
Luca Tamagnini <wiiuforever25@gmail.com>
2024-05-09 10:16:44 +0000

    Author

--- project/blogs/models.py
+++ project/blogs/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
+
 # Create your models here.
 class post(models.Model):
+    author = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.TextField(max_length=200)
     content = models.TextField(max_length=7500)
 
