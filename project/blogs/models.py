e51d1df69f5208181dde49bdea05dd619070ead7
Luca Tamagnini <wiiuforever25@gmail.com>
2024-05-09 10:14:30 +0000

    Post model

--- project/blogs/models.py
+++ project/blogs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class post(models.Model):
+    title = models.TextField(max_length=200)
+    content = models.TextField(max_length=7500)
+
+    def __str__(self):
+        return self.title
